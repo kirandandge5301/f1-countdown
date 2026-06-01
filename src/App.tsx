c9b85a54eddc92ec0637737d20feca99e4983973
kirandandge5301 <36322060+kirandandge5301@users.noreply.github.com>
2026-06-01 11:54:36 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,29 @@
 import { ThemeProvider } from '@/context/ThemeContext';
 import { TimezoneProvider } from '@/context/TimezoneContext';
+import { DataProvider } from '@/context/DataContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import NextRaceSection from '@/sections/NextRaceSection';
 import ScheduleSection from '@/sections/ScheduleSection';
 import HowToWatchSection from '@/sections/HowToWatchSection';
 import StandingsSection from '@/sections/StandingsSection';
-import { useEffect } from 'react';
-import { fetchCurrentSchedule } from './f1api';
-function App() {useEffect(() => { 
-  fetchCurrentSchedule()
-  .then(data => {
-    console.log('F1 API:', data);
 
-    const races = data.MRData.RaceTable.Races;
-
-    console.log('FIRST RACE:', races[0]);
-console.log('LAST RACE:', races[races.length - 1]);
-
-const upcoming = races.find(
-  (race: any) => new Date(race.date) > new Date()
-);
-
-console.log('NEXT UPCOMING RACE:', upcoming);
-  })
-    .catch(error => {
-      console.error(error);
-    });
-}, []);
+function App() {
   return (
     <ThemeProvider>
       <TimezoneProvider>
-        <div className="min-h-screen bg-[var(--bg-primary)] text-[var(--text-primary)] transition-colors duration-300">
-      
-          <Header />
-          <main>
-            <NextRaceSection />
-            <ScheduleSection />
-            <HowToWatchSection />
-            <StandingsSection />
-          </main>
-          <Footer />
-        </div>
+        <DataProvider>
+          <div className="min-h-screen bg-[var(--bg-primary)] text-[var(--text-primary)] transition-colors duration-300">
+            <Header />
+            <main>
+              <NextRaceSection />
+              <ScheduleSection />
+              <HowToWatchSection />
+              <StandingsSection />
+            </main>
+            <Footer />
+          </div>
+        </DataProvider>
       </TimezoneProvider>
     </ThemeProvider>
   );
